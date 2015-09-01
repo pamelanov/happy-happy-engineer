e44ea702f098acdd6cd0120c7ddc2cae7ba27aa8
pamelanov <pamelanovranska@Pamelas-MacBook-Pro.local>
2015-09-01 22:42:38 +0700

    tambah file

---
+++ application/views/home.php
@@ -0,0 +1,47 @@
+<div id="homepage">
+  <div class="row" id="pengganti_slideshow">
+
+    <a href="#" class="thumbnail">
+      <img src="<?php echo base_url();?>pictures/slideshow.png" alt="foto_slideshow">
+    </a>
+
+</div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="<?php echo base_url();?>pictures/H2E_1488.jpg" alt="girl section">
+      <div class="caption">
+        <h3>Browse Girls Section</h3>
+        <p>Explore our girl collection and get discounts for early buyers!</p>
+        <p><a href="#" class="btn btn-danger" role="button">Browse</a>
+
+      </div>
+    </div>
+  </div>
+    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="<?php echo base_url();?>pictures/H2E_6849.jpg" alt="boy section">
+      <div class="caption">
+        <h3>Browse Boys Section</h3>
+        <p>Explore our boys collection and get discounts for early buyers!</p>
+        <p><a href="#" class="btn btn-danger" role="button">Browse</a>
+
+      </div>
+    </div>
+  </div>
+	    <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="<?php echo base_url();?>pictures/H2E_4374.jpg" alt="boy section">
+      <div class="caption">
+        <h3>Browse Boys Section</h3>
+        <p>Explore our boys collection and get discounts for early buyers!</p>
+        <p><a href="#" class="btn btn-danger" role="button">Browse</a>
+		
+      </div>
+    </div>
+  </div>
+</div>
+  
+
+</div>
