44a3b03fe81197266748d1d5c9acbab6dbd885ba
arvinsamuar <samuar.arvin@gmail.com>
2015-09-08 13:44:29 +0700

    test
    lupa update apaan

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -5,18 +5,19 @@
       <img src="<?php  base_url();?>pictures/wallpaper_jeans.png" alt="foto_slideshow">
     </a>
  -->
-  <div class= "row" id="contact_us_pict">
+  <div class= "row" id="contact_us_order">
     <div class="col-sm-6 col-md-4 col-xs-6">
       <!-- harusnya gambar order -->
-      <img src ="<?php echo base_url();?>pictures/brand_logo_resized.jpg" alt="picture">
+      <img src ="<?php echo base_url();?>pictures/order.png" alt="order">
      </div> 
     <div class="col-sm-8 col-md-8 col-xs-12">
       <div id="tulisan">
         <!-- harusnya informasi ordernya dimana -->
-        <h2> blablablablablablablbalbla </h2>   
+        <h3> Line Order @h2e </h3>   
       </div>
     </div>
-
+  </div>  
+  <div class= "row" id="contact_us_ig">
     <div class="col-sm-6 col-md-4 col-xs-6">
       <!-- harusnya gambar ig -->
       <img src ="<?php echo base_url();?>pictures/brand_logo_resized.jpg" alt="picture">
@@ -27,7 +28,8 @@
         <h2> blablablablablablablbalbla </h2>   
       </div>
     </div>
-
+  </div>
+  <div class= "row" id="contact_us_alamat">
     <div class="col-sm-6 col-md-4 col-xs-6">
       <!-- harusnya gambar rumah(alamat toko) -->
       <img src ="<?php echo base_url();?>pictures/brand_logo_resized.jpg" alt="picture">
