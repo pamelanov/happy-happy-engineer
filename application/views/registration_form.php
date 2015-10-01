0866f43fe8da6140654eff5af6780d7a343f9344
arvinsamuar <samuar.arvin@gmail.com>
2015-10-01 08:11:10 +0700

    nambahin login
    belum ada database

--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -1,7 +1,7 @@
 <html>
 	<?php
 		if (isset($this->session->userdata['logged_in'])) {
-		header("location: http://localhost/login/index.php/user_authentication/user_login_process");
+			header("location: http://localhost/login/index.php/user_authentication/user_login_process");
 		}
 	?>
 <head>
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
 </head>
+
 <body>
 	<div id="main">
 		<div id="login">
@@ -16,28 +17,25 @@
 			<hr/>
 			<?php
 				echo "<div class='error_msg'>";
-					echo validation_errors();
-					echo "</div>";
+				echo validation_errors();
+				echo "</div>";
 				echo form_open('user_authentication/new_user_registration');
 
 				echo form_label('Create Username : ');
 				echo"<br/>";
 				echo form_input('username');
-				
 				echo "<div class='error_msg'>";
-					if (isset($message_display)) {
-						echo $message_display;
-						}
+				if (isset($message_display)) {
+					echo $message_display;
+				}
 				echo "</div>";
 				echo"<br/>";
-				
 				echo form_label('Email : ');
 				echo"<br/>";
-					$data = array(
-					'type' => 'email',
-					'name' => 'email_value'
-					);
-				
+				$data = array(
+				'type' => 'email',
+				'name' => 'email_value'
+				);
 				echo form_input($data);
 				echo"<br/>";
 				echo"<br/>";
@@ -49,7 +47,7 @@
 				echo form_submit('submit', 'Sign Up');
 				echo form_close();
 			?>
-		<a href="<?php echo base_url() ?> ">For Login Click Here</a>
+			<a href="<?php echo base_url() ?> ">For Login Click Here</a>
 		</div>
 	</div>
 </body>
