65ef04defdebb1af9e2cea7eab3d05889bdbe80e
arvinsamuar <samuar.arvin@gmail.com>
2016-02-10 21:39:42 +0700

    rapih

--- application/views/gallery_view.php
+++ application/views/gallery_view.php
@@ -44,17 +44,17 @@
 
 <script language="text/javascript">
   
-   $(document).ready(function(){
-           $('li img').on('click',function(){
-                var src = $(this).attr('src');
-                var img = '<img src="' + src + '" class="img-responsive"/>';
-                $('#myModal').modal();
-                $('#myModal').on('shown.bs.modal', function(){
-                    $('#myModal .modal-body').html(img);
-                });
-                $('#myModal').on('hidden.bs.modal', function(){
-                    $('#myModal .modal-body').html('');
-                });
-           });
-        })
+  $(document).ready(function(){
+    $('li img').on('click',function(){
+      var src = $(this).attr('src');
+      var img = '<img src="' + src + '" class="img-responsive"/>';
+        $('#myModal').modal();
+        $('#myModal').on('shown.bs.modal', function(){
+          $('#myModal .modal-body').html(img);
+        });
+        $('#myModal').on('hidden.bs.modal', function(){
+          $('#myModal .modal-body').html('');
+        });
+     });
+  })
 </script>
